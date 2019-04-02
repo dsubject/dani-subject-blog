b966c4ab32e38344076e809407d806f0d1517399
dsubject <daniellesubject@gmail.com>
2019-04-02 15:48:41 -0400

    fixed banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -14,6 +14,7 @@ const Banner = class extends React.Component {
           backgroundImage: `url(${
             banner
           })`,
+          backgroundRepeat: 'no-repeat'
         }}
       />
     </div>
